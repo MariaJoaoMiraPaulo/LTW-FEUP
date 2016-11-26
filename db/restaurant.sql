fc33242a171e51c87a40643a6f456ba2b92be3bb
Pedro Costa <pedrodc23@gmail.com>
2016-11-26 19:27:06 +0000

    added services div

--- db/restaurant.sql
+++ db/restaurant.sql
@@ -30,7 +30,7 @@ CREATE TABLE comments (
 
 
 INSERT INTO restaurant VALUES (NULL, 'Gull', 'Rua Cais das Pedras,15 4050-46','Porto','http://www.gull.pt/',('Almoço','Jantar'),('Sushi'),' 12:30 to 18:00, 19:30 to 01:00','€70 for two people','4');
-INSERT INTO restaurant VALUES (NULL, 'Cafeína', 'Rua do Padrão, 100, Foz','Porto','http://www.cafeina.pt/',('Almoço','Jantar'),('Suhi','Mediterrânia'),' 12:30 to 18:00, 19:30 to 01:00','€70 for two people','4.5');
+INSERT INTO restaurant VALUES (NULL, 'Cafeína', 'Rua do Padrão, 100, Foz','Porto','http://www.cafeina.pt/',('Almoço','Jantar'),('Sushi','Mediterrânia'),' 12:30 to 18:00, 19:30 to 01:00','€70 for two people','4.5');
 INSERT INTO restaurant VALUES (NULL, 'Portarossa', 'Rua de Corte Real, 289, Foz','Porto','http://www.cafeina.pt/pt/Portarossa',('Almoço','Jantar'),('Mediterrânia'),' 12:30 to 18:00, 19:30 to 01:00','€40 for two people','4');
 
 
