7bad2622c8da522bf76b768b1bd20742b7646b26
Pedro Costa <pedrodc23@gmail.com>
2016-11-26 23:22:16 +0000

    implemented footer and header in php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<!-- begin footer -->
+
+
+<footer>
+    <div class="footer">
+
+    </div>
+</footer>
+</html>
---
+++ header.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head >
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,700,400' rel='stylesheet' type='text/css'>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <script src="js/main.js"></script> <!-- Resource jQuery -->
+    <link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+    <link rel="stylesheet" href="css/style.css"> <!-- Resource style -->
+    <div class="header">
+    <span class="header-logo">
+
+    </span>
+    </div>
+</head>
