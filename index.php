c95feaf5d6da6541585202281766841de13267c4
Pedro Costa <pedrodc23@gmail.com>
2016-11-29 15:03:17 +0000

    updated mask

--- index.php
+++ index.php
@@ -13,29 +13,29 @@ include_once "header.php";
                 <path
                         d="M1836.9,1488h-2.9v24h2.1c3.5,0,6.1-1,7.7-3.1s2.5-5.2,2.5-9.4c0-3.9-0.8-6.8-2.3-8.7   C1842.5,1488.9,1840.1,1488,1836.9,1488z"
                         id="path2"
-                        fill="#FFFFFF" /><path
+                        fill="#212121" /><path
                         d="M 0,0 V 3000 H 4000 V 0 Z"
                         id="path4"
                         inkscape:connector-curvature="0"
-                        style="fill:#ffffff"
+                        style="fill:#212121"
                         sodipodi:nodetypes="ccccc" /><path
                         d="M1942.3,1487.4c-5.8,0-8.7,4.2-8.7,12.5c0,8.3,2.9,12.4,8.6,12.4c2.9,0,5.1-1,6.5-3c1.4-2,2.1-5.1,2.1-9.4   c0-4.3-0.7-7.4-2.2-9.5C1947.3,1488.5,1945.1,1487.4,1942.3,1487.4z"
                         id="path6"
-                        fill="#FFFFFF" />
+                        fill="#212121" />
             </svg> <!-- .svg-mask -->
 
             <svg class="svg-mask-mobile" x="0px" y="0px" width="2000px" height="3000px" viewBox="0 0 2000 3000">
                 <title>El Capitáno Ristorante</title>
                 <path
-                        fill="#FFFFFF"
+                        fill="#212121"
                         d="M1082,1445c-5.8,0-8.7,4.2-8.7,12.5s2.9,12.4,8.6,12.4c2.9,0,5.1-1,6.5-3s2.1-5.1,2.1-9.4s-0.7-7.4-2.2-9.5  C1087,1446,1084.9,1445,1082,1445z"
                         id="path2" /><path
                         d="M 0,0 V 3000 H 2000 V 0 Z"
                         id="path4"
                         inkscape:connector-curvature="0"
-                        style="fill:#ffffff"
+                        style="fill:#212121"
                         sodipodi:nodetypes="ccccc" /><path
-                        fill="#FFFFFF"
+                        fill="#212121"
                         d="M993.2,1466.4c1.6-2,2.5-5.2,2.5-9.4c0-3.9-0.8-6.8-2.3-8.7c-1.6-1.9-3.9-2.8-7.1-2.8h-3.4v24h2.6  C989,1469.5,991.6,1468.5,993.2,1466.4z"
                         id="path6" />
             </svg> <!-- .svg-mask-mobile -->
