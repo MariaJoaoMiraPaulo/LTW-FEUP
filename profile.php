652cd0ebb8d74ecd90bc649ae302b97168398646
MariaJoaoMiraPaulo <mariajoaomirapaulo1996@gmail.com>
2016-11-29 12:30:40 +0000

    Starting Profile

--- profile.php
+++ profile.php
@@ -2,7 +2,13 @@
 $title = "Welcome";      // Set the title
 include "header.php";
 ?>
-    <button class="button" type="button" onclick="location.href='index.php';">Exit</button>
+    <div class="center">
+        <img class="img-circle" src="assets/profile-icon.png">
+        <button type="button" onclick="location.href='index.php';">Exit</button>
+    </div>
+
+
+
 
 <?php
 include "footer.php";
