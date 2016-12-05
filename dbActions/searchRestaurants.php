75124802fdc47d7baf90a0c813af149544fc9578
Pedro Costa <pedrodc23@gmail.com>
2016-12-05 13:34:26 +0000

    :map: search implemented

--- dbActions/searchRestaurants.php
+++ dbActions/searchRestaurants.php
@@ -2,8 +2,22 @@
 
 include_once ('restaurantUtils.php');
 
-
-if(preg_match ("/[a-zA-Z]/", $_POST['restaurant'])){
-    $name=$_POST['restaurant'];
-    getRestaurantIdFromName($name);
+if(isset($_POST['submit'])) {
+    if (preg_match("/[a-zA-Z]/", $_POST['restaurant'])) {
+        $name = $_POST['restaurant'];
+        $result = getRestaurantIdFromName($name);
+        foreach($result as $row){
+            $restaurantName = $row['name'];
+            echo "<ul>\n";
+            echo "<li>"   .$restaurantName . "</a></li>\n";
+            echo "</ul>";
+        }
+        $result = getRestaurantIdFromCategory($name);
+        foreach($result as $row){
+            $restaurantName = $row['name'];
+            echo "<ul>\n";
+            echo "<li>"   .$restaurantName . "</a></li>\n";
+            echo "</ul>";
+        }
+    }
 }
