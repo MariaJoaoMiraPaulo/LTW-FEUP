5ed9b2ba85070e6adc337400c10447a1ede0a54d
Pedro Costa <pedrodc23@gmail.com>
2016-12-07 10:33:30 +0000

    updated restaurantes page

--- pages/searchRestaurants.php
+++ pages/searchRestaurants.php
@@ -3,29 +3,6 @@ $title = "Restaurants";      // Set the title
 
 include_once('../dbActions/restaurantUtils.php');
 include "header.php";
-
-if(isset($_POST['submit'])) {
-    if (preg_match("/[a-zA-Z]/", $_POST['restaurant'])) {
-        $name = $_POST['restaurant'];
-        $result = getRestaurantIdFromName($name);
-        foreach($result as $row){
-            $restaurantName = $row['name'];
-            echo "<ul>\n";
-            echo "<li>"   .$restaurantName . "</a></li>\n";
-            echo "</ul>";
-        }
-        $result = getRestaurantIdFromCategory($name);
-        foreach($result as $row){
-            $restaurantName = $row['name'];
-            echo "<ul>\n";
-            echo "<li>"   .$restaurantName . "</a></li>\n";
-            echo "</ul>";
-        }
-    }
-}
-
-
-
 ?>
     <!DOCTYPE html>
 
@@ -33,25 +10,38 @@ if(isset($_POST['submit'])) {
     <main class="cd-main-content">
         <div class="cd-tab-filter-wrapper">
             <div class="cd-tab-filter">
-                <form method="post" action="searchRestaurants.php" class="action-wrapper">
+                <!-- <form method="post" action="searchRestaurants.php" class="action-wrapper">
                     <input class="select-location" type="text" name="search" placeholder="Location">
                     <input class="search-bar" type="text" name="restaurant" placeholder="Search for restaurants or cuisines...">
                     <input class="button" type="submit" name="submit" value="Search">
-                </form>
+                </form>-->
             </div> <!-- cd-tab-filter -->
         </div> <!-- cd-tab-filter-wrapper -->
 
         <section class="cd-gallery">
             <ul>
-                <li class="mix color-1 check1 checkbox2 option3"><img src="../assets/img-1.jpg" alt="Image 1"></li>
-                <li class="mix color-2 check2 checkbox2 option2"><img src="../assets/img-2.jpg" alt="Image 2"></li>
-                <li class="mix color-1 check1 checkbox2 option3"><img src="../assets/img-1.jpg" alt="Image 1"></li>
-                <li class="mix color-2 check2 checkbox2 option2"><img src="../assets/img-2.jpg" alt="Image 2"></li>
-                <li class="mix color-2 check2 checkbox2 option2"><img src="../assets/img-2.jpg" alt="Image 2"></li>
-                <li><!-- ... --></li>
+                <?php
+                if(isset($_POST['submit'])) {
+                    if (preg_match("/[a-zA-Z]/", $_POST['restaurant'])) {
+                        $name = $_POST['restaurant'];
+                        $result = getRestaurantIdFromName($name);
+                        foreach($result as $row){
+                            $restaurantName = $row['name'];
+                            echo "<li class='item'>";
+                            echo "<h1>" .$restaurantName."</h1>";
+
+                            echo "</li>\n";
+                        }
+                        $result = getRestaurantIdFromCategory($name);
+                        foreach($result as $row){
+                            $restaurantName = $row['name'];
+                            echo "<li>"   .$restaurantName . "</a></li>\n";
+                        }
+                    }
+                }
+                ?>
                 <li class="gap"></li>
             </ul>
-            <div class="cd-fail-message">No results found</div>
         </section> <!-- cd-gallery -->
 
         <div class="cd-filter">
